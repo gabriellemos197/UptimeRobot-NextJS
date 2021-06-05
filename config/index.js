a90b5c06422c4351b9b0831216da5e174bcc1f27
gabriellemos197 <contato@gabriellemos.com>
2021-06-05 11:32:35 -0300

    ajuste api

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c95";
+let uptimeRobotToken = "u683846-cdbb17cbd5416d1c0b5d0116";
 // let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c5";
 
 export { uptimeRobotToken };
